4c1285029fec337b78f9fe5b5662dec15be59681
dewo3276 <44533218+dewo3276@users.noreply.github.com>
2021-03-04 07:57:56 -0700

    Add files via upload

---
+++ Art Collector/Art Collector/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  Art Collector
+//
+//  Created by Dusty on 3/4/21.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var artImages: UIImageView!
+}

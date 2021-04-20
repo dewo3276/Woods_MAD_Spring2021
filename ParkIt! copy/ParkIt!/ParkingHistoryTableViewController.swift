e98ca4820c920a8bc3d62a71eeb6d1770c291c47
dewo3276 <44533218+dewo3276@users.noreply.github.com>
2021-04-20 09:00:13 -0600

    Add files via upload

---
+++ ParkIt! copy/ParkIt!/ParkingHistoryTableViewController.swift
@@ -0,0 +1,150 @@
+//
+//  ParkingHistoryTableViewController.swift
+//  ParkIt!
+//
+//  Created by Dusty on 2/22/21.
+//
+
+import UIKit
+
+class ParkingHistoryTableViewController: UITableViewController {
+    
+    var parkingData = parkingDataLoader()
+    var dateHistory = [String]()
+    var locationHistory = [String]()
+    
+    let formatter: DateFormatter =
+    {
+        let formatter = DateFormatter()
+        formatter.timeZone = .current
+        formatter.dateStyle = .short
+        formatter.timeStyle = .short
+        formatter.locale = .current
+        return formatter
+        
+    }()
+
+    override func viewWillAppear(_ animated: Bool) {
+        parkingData.loadData()
+        dateHistory=parkingData.getDate()
+        locationHistory=parkingData.getLocation()
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        parkingData.loadData()
+        dateHistory=parkingData.getDate()
+        locationHistory=parkingData.getLocation()
+        //print(locationHistory)
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return dateHistory.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "parkingHistory", for: indexPath)
+
+        cell.textLabel?.text=dateHistory[indexPath.row]
+        cell.detailTextLabel?.text=locationHistory[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        Varriables.selectedCoords[0]=(locationHistory[(indexPath.row)*2])
+        Varriables.selectedCoords[1]=(locationHistory[((indexPath.row)*2)+1])
+        Varriables.didAppearFromTableView=true
+        Varriables.selectedDate=dateHistory[indexPath.row]
+        tabBarController?.selectedIndex = 0
+    }
+    
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .delete
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            tableView.beginUpdates()
+            
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            parkingData.deleteData(index: indexPath.row)
+            
+            tableView.endUpdates()
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+ */
+    
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ParkIt! copy/mapWidget/dataProvider.swift
@@ -0,0 +1,8 @@
+//
+//  dataProvider.swift
+//  ParkIt!
+//
+//  Created by Dusty on 4/20/21.
+//
+
+import Foundation

954bd87a039affe1084062cab45b5f6168216e6e
dewo3276 <44533218+dewo3276@users.noreply.github.com>
2021-04-29 09:57:55 -0600

    Add files via upload

---
+++ ParkIt! copy 3/ParkIt!/globalVarriables.swift
@@ -0,0 +1,14 @@
+//
+//  globalVarriables.swift
+//  ParkIt!
+//
+//  Created by Dusty on 3/23/21.
+//
+
+import Foundation
+
+struct Varriables{
+    static var selectedCoords = ["0","0"]
+    static var didAppearFromTableView = false
+    static var selectedDate = "No Date selected"
+}
